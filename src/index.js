4871046e0cc17d8917a9dc8dd35a4f8feb961fd8
pbhagyashri <bhagyashri.padmawar@sideinc.com>
2023-01-11 03:14:03 -0500

    change to prod url

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+	uri: 'https://odyssey-lift-off-part5-server-production-e2d2.up.railway.app/', // change to YOUR own production server
+	cache: new InMemoryCache(),
+	name: 'web',
+	version: '1.0',
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
-  document.getElementById("root")
+	<ApolloProvider client={client}>
+		<GlobalStyles />
+		<Pages />
+	</ApolloProvider>,
+	document.getElementById('root'),
 );
